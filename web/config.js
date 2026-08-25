a21c98592a48957305e1c143ba5c2c868a26d403
infomoufit-prog <infomoufit@gmail.com>
2026-08-25 11:14:44 +0200

    build 20080: align client release for Finance Premium

--- web/config.js
+++ web/config.js
@@ -12,7 +12,7 @@ window.UW_CONFIG = {
   },
   release: {
     version: '2.0.0-rc.13',
-    build: 20078,
+    build: 20080,
     backendVersion: '1.6.0',
     schemaEpoch: 160,
     mutationEndpoint: 'app_mutate_v160',
